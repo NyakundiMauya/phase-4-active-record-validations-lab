76b44cce604671c6a66b7bd741ee8c0ca6b6444b
NyakundiMauya <n.mauya.nm@gmail.com>
2023-07-09 14:05:28 +0300

    done

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,15 @@
 class Author < ApplicationRecord
+    validates :name, presence: true
+validates :name, uniqueness: { case_sensitive: false }
+validates :phone_number, length: { is: 10 }, numericality: { only_integer: true }
+validates :title, presence: true
+
+validate :case_sensitive_name_uniqueness
+
+def case_sensitive_name_uniqueness
+  if Author.exists?(name: name)
+    errors.add(:name, "has already been taken")
+  end
+end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+
+    validate :clickbait_title
+
+    def clickbait_title
+      clickbait_phrases = ["Won't Believe", "Secret", "Top", "Guess"]
+      unless clickbait_phrases.any? { |phrase| title.to_s.include?(phrase) }
+        errors.add(:title, "must be clickbait")
+      end
+    end
+    
 end
